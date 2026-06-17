6116271306f83ce0d30b3f3bed4f930c78b99c7d
djabranemmd <mammadi.a.djabrane@gmail.com>
2026-06-17 01:09:50 +0100

    Add smooth page transitions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import {
+  AnimatePresence,
+  motion,
+} from "framer-motion";
+
+
 import AuroraBackground from "../components/layout/AuroraBackground";
 import DashboardLayout from "../components/layout/DashboardLayout";
 import TopBar from "../components/layout/TopBar";
@@ -7,47 +13,70 @@ import Statistics from "../components/stats/Statistics";
 import DashboardExtras from "../components/stats/DashboardExtras";
 import AudioMixer from "../components/audio/AudioMixer";
 
+
 import {
   useNavigation,
 } from "../context/NavigationContext.jsx";
 
 
+
 function Home() {
+
+
   const {
     activePage,
   } = useNavigation();
 
 
+
+
   const renderContent = () => {
+
 
     switch(activePage) {
 
+
       case "focus":
+
         return (
           <TimerSection />
         );
 
 
+
       case "sounds":
+
         return (
           <AudioMixer />
         );
 
 
+
       case "statistics":
+
         return (
+
           <>
+
             <Statistics />
 
             <DashboardExtras />
+
           </>
+
         );
 
 
+
       case "dashboard":
+
+      // eslint-disable-next-line no-fallthrough
       default:
+
         return (
+
           <>
+
             <TimerSection />
 
             <Statistics />
@@ -55,38 +84,103 @@ function Home() {
             <DashboardExtras />
 
             <AudioMixer />
+
           </>
+
         );
+
+
     }
+
   };
 
 
+
+
+
   return (
+
     <>
+
       <AuroraBackground />
+
+
 
       <DashboardLayout>
 
+
         <main
-  className="
-    w-full
-    max-w-full
-    overflow-hidden
-    p-4
-    sm:p-6
-    lg:p-10
-  "
->
+          className="
+            w-full
+            max-w-full
+            overflow-hidden
+            p-4
+            sm:p-6
+            lg:p-10
+          "
+        >
+
 
           <TopBar />
 
-          {renderContent()}
+
+
+          <AnimatePresence
+            mode="wait"
+          >
+
+            <motion.div
+
+              key={activePage}
+
+
+              initial={{
+                opacity: 0,
+                y: 15,
+              }}
+
+
+              animate={{
+                opacity: 1,
+                y: 0,
+              }}
+
+
+              exit={{
+                opacity: 0,
+                y: -10,
+              }}
+
+
+              transition={{
+                duration: 0.3,
+                ease: "easeOut",
+              }}
+
+            >
+
+              {
+                renderContent()
+              }
+
+
+            </motion.div>
+
+
+          </AnimatePresence>
+
+
 
         </main>
 
+
       </DashboardLayout>
+
+
     </>
+
   );
+
 }
 
 
